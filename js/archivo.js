d999886003948b0d7939cbbfe6e1c392a2a3b561
victor <victorgonzalez108159@correo.itm.edu.co>
2022-09-16 21:01:56 -0500

    Inicio del Proyecto

---
+++ js/archivo.js
@@ -0,0 +1,12 @@
+function saludar() {
+    alert("Hola");
+//setinterval sirve para ejecutar secuencias de funciones    
+}
+//setInterval(saludar,1000,3000)
+
+//otra forma son las funciones anonimas 7 funciones flechas
+//setInterval(function(){alert("Hola")},3000)
+
+//setInterval(()=> alert("Hola"),3000)
+
+//setTimeout se ejecuta las veces 
